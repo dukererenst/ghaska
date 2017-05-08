02ae59bfec794cfd4b75814c6ad4abcc6d9be2d3
dukererenst <dukerernestjunior@gmail.com>
2017-05-08 23:14:04 +0900

    Initial Commit

--- resources/views/include/breadcrumb.blade.php
+++ resources/views/include/breadcrumb.blade.php
@@ -2,7 +2,7 @@
         <div class="container">
             <ol class="breadcrumb">
                 <li>
-                    <a href="index.html"> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Home
+                    <a href='{{route('home')}}'> <i class="livicon icon3 icon4" data-name="home" data-size="18" data-loop="true" data-c="#3d3d3d" data-hc="#3d3d3d"></i>Home
                     </a>
                 </li>
                 <li class="hidden-xs">
